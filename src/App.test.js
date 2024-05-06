45f331e36ae3d01b7b8c7fa948647749298bdde2
annavlasenko <anna.a.vlasenko@gmail.com>
2024-05-06 16:06:24 +1000

    add some comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
-
+// this project has no tests, so this is just a placeholder test
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
