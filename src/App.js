d0dbf33d433ce71cd13d1900532a26cef7bc332b
annavlasenko <anna.a.vlasenko@gmail.com>
2024-05-06 11:11:03 +1000

    prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import Home from "./pages/Home";
-import {QueryClient} from "@tanstack/react-query";
-import {QueryClientProvider} from "@tanstack/react-query";
-import { MantineProvider } from '@mantine/core'
+import Home from './pages/Home';
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { MantineProvider } from '@mantine/core';
 function App() {
-    const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <MantineProvider>
-        <QueryClientProvider client={queryClient}>
-      <Home/>
-        </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
